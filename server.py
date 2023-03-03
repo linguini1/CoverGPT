1ca2ac36469ee7deda47a7a98f1e873dfadf16f6
Matteo Golin <matteo.golin@gmail.com>
2023-03-02 21:02:43 -0500

    Initial web application setup

---
+++ server.py
@@ -0,0 +1,15 @@
+# The Flask server for the CoverGPT web application
+__author__ = "Matteo Golin"
+
+# Imports
+
+# Constants
+
+
+# Main
+def main():
+    pass
+    
+    
+if __name__ == "__main__":
+    main()
